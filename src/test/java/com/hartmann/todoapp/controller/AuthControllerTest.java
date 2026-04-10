568e44784938e98ed1a77d28a59327cd31c3743e
Thomas Hartmann <thomas@hartmann.dev>
2026-04-10 09:23:56 +0200

    fix: replace @AutoConfigureMockMvc with manual MockMvc setup (Spring Boot 4)
    Author: Thomas Hartmann

--- src/test/java/com/hartmann/todoapp/controller/AuthControllerTest.java
+++ src/test/java/com/hartmann/todoapp/controller/AuthControllerTest.java
@@ -1,20 +1,32 @@
 package com.hartmann.todoapp.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class AuthControllerTest {
 
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
 
     @Test
     void loginPageShouldReturn200() throws Exception {
